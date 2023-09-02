33ee1b0c89cd937dc8fa2af15fdd6aae62c6e788
unbii <143859355+unbii@users.noreply.github.com>
2023-09-02 10:43:05 +0700

    commit pertama

---
+++ h1.py
@@ -0,0 +1,32 @@
+# apel = 52.2
+# print(apel)
+# print(521)
+# nama_apel = "apel malang"
+# print (nama_apel)
+# print (type(nama_apel))
+# apel_benar = true 
+# apel_salah = False
+# print(type(apel_benar))
+# print(type(apel_salah))
+
+
+
+# list_apel = ['hijau','manis']
+# list_apel = ["manis"]
+
+# tuple_jumlah = {2, 5.25,"satu"}
+# print(tuple_jumlah)
+
+# apel_dictionary = {'nama':}
+
+# print(34+2)
+# print(39)
+# print(2*6)
+
+b = 39
+e = 4 
+print(b // e)
+
+
+
+
